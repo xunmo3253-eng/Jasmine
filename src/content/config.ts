8d827640b1e5931cc51eb936c6ea6233fce12904
Your Name <your-email@example.com>
2026-06-21 00:32:08 +0800

    將規則頁面改為Markdown管理

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,28 +1,5 @@
----
-import Layout from '../layouts/Layout.astro';
-import { getEntry } from 'astro:content';
-
-// 這行指令會去把我們剛剛寫的筆記抓出來
-const entry = await getEntry('promise', 'my-promise');
-const { Content } = await entry.render();
----
-
-<Layout>
-  <div class="content">
-    <Content />
-    
-    <a href="/Jasmine.memory" class="back-link">回到夢境時光</a>
-  </div>
-</Layout>
-
-<style>
-  .content { 
-    text-align: center; 
-    max-width: 600px; 
-    margin: 0 auto; 
-    padding-top: 10vh; 
-    color: white; 
-    line-height: 1.8; /* 讓文字行距舒服一點，像書一樣 */
-  }
-  .back-link { color: #ffd1dc; }
-</style>
+import { defineCollection } from 'astro:content';
+export const collections = {
+  'promise': defineCollection({ type: 'content' }),
+  'rules': defineCollection({ type: 'content' }), // 加上這行
+};
